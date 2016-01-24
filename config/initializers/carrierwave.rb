9cfd1076b1c685d18dfa0352c7f320c8ec409d20
Justin Joseph <justin.joseph0@yahoo.com>
2016-01-24 21:26:38 +0000

    Attempt to fix S3 connection

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,6 @@ CarrierWave.configure do |config|
     :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
     :aws_secret_access_key => ENV['AWS_SECRET_KEY']
   }
-  
+
   config.fog_directory = ENV['AWS_BUCKET']
 end
